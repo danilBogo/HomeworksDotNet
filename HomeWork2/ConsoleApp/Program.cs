4cdf64f5e08c5de9f7b6397e4abd7a7f096386d3
Daniil Bogomolov <d.bogomolov@ext.hightech.group>
2022-07-06 09:58:31 +0300

    add HomeWork2

---
+++ HomeWork2/ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using IlClasses;
+
+var check = Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
+if (check != 0) return check;
+var result = Calculator.Calculate(val1, operation, val2);
+Console.WriteLine($"Result is: {result}");
+return 0;
